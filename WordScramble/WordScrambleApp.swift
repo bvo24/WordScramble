d6a9bfb37ce3b57cb269cfd1ef87add6f7b96155
bvo24 <brianvo2020@gmail.com>
2024-04-29 23:18:05 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Brian Vo on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
